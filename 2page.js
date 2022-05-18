44f381ec6ddb906ee704443272995c91682e5977
Saee11 <99674826+Saee11@users.noreply.github.com>
2022-05-18 14:03:56 +0530

    Add files via upload

---
+++ 2page.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDt0FDpks9urhxBgyk9yiqUVut3IvmlCHY",
+    authDomain: "p93-09876.firebaseapp.com",
+    projectId: "p93-09876",
+    storageBucket: "p93-09876.appspot.com",
+    messagingSenderId: "555135504950",
+    appId: "1:555135504950:web:cfc930e48eb2358c98fe97",
+    measurementId: "G-5PMRZ6ZHXE"
+  };
+  
